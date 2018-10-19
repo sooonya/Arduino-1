aac2a15c16dfad8a9449891aa5947853fdc72d4c
89apt89 <89apt89@protonmail.ch>
2018-10-19 12:10:27 -0400

    add comments and refactor a bit

--- Knight Rider/sketch.ino
+++ Knight Rider/sketch.ino
@@ -1,25 +1,35 @@
-int pins[] = {2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2};
+int pins[] = {2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2}; // pin animation
 int arrSize = sizeof(pins) / sizeof(int);
 
+bool enablePotentiometer = true;
+int potentiometerPin = 0;
+int delayms = 100;
+
 void setup() {
+  // loop through pin array and initialize them
   for (int i = 0; i < arrSize; i++) {
     pinMode(pins[i], OUTPUT);
   }
-  thread
 }
 
 void loop() {
+  // loop through each pin in array and write high
   for (int i = 0; i < arrSize; i++) {
     digitalWrite(pins[i], HIGH);
-    //digitalWrite(pins[i+1], HIGH);
+    //digitalWrite(pins[i+1], HIGH); // uncomment this if you want two lights at once
     if (i != 0) {
-      delay(analogRead(0));
+      if (enablePotentiometer) {
+        delay(analogRead(0));
+      } else {
+        delay(delayms);
+      }
     }
     resetPins();
   }
 }
 
 void resetPins() {
+  // loop through each pin in array and write low 
   for (int i = 0; i < arrSize; i++) {
     digitalWrite(pins[i], LOW);
   }
